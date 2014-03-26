85b9b82db67b7df26d0b85663a3adf56ea6f22d9
Ishikawa <iikuni.oyama@gmail.com>
2014-03-26 11:11:37 +0900

    pullするためにcommit

--- app.js
+++ app.js
@@ -158,7 +158,7 @@ var play_socket = io.of('/LocalInvaders/play').on('connection', function(socket)
   socket.on("C_to_S_game_start", function () {
 	//次のゲームが始められるようにフラグを折る
 	share.timer = false;
-    //setTimeout(function(){play_socket.emit("S_to_C_game_end")}, 30000);
+    setTimeout(function(){play_socket.emit("S_to_C_game_end")}, 1500000);
 	var location = share.new_area();
     play_socket.emit("S_to_C_create_new_area", {location:location})
   });
